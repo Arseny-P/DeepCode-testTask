1ab1bc14701c1c61ef15577f252f5eee523f3c36
Arseniy <catdog09073@gmail.com>
2026-05-10 17:35:17 +0300

    feat: добавить скелетоны для карточки пользователя/поста

--- src/pages/singlePost/SinglePost.tsx
+++ src/pages/singlePost/SinglePost.tsx
@@ -1,44 +1,41 @@
 import { getSinglePost } from '@/shared/api/hooks/getSinglePost'
 import { useNavigate, useParams } from 'react-router';
-import { Loader } from '@consta/uikit/Loader';
 import Heading from '@/shared/ui/Heading';
 import { Text } from '@consta/uikit/Text';
 import { Button } from '@consta/uikit/Button';
 import { Layout } from '@consta/uikit/Layout';
 import { Card } from '@consta/uikit/Card';
 import { getSingleUser } from '@/shared/api/hooks/getSingleUser';
-
-type Comment = {
-    id: number,
-    post_id: number,
-    name: string,
-    email: string,
-    body: string
-}
-
-type Post = {
-    id: number,
-    user_id: number,
-    title: string,
-    body: string
-}
+import { IconArrowUndone } from '@consta/icons/IconArrowUndone';
+import ErrorPage from '../error/ErrorPage';
+import SinglePostSkeleton from '@/shared/ui/SinglePostSceleton';
+import type { Comment } from '@/entities/post/post';
 
 const SinglePost = () => {
   const navigate = useNavigate();
   let params = useParams();
   const {data: post, isLoading: isPostLoading, isError: isPostError} = getSinglePost(Number(params.postId));
 
-  if(isPostLoading) {
-    return <Loader />
+  const postData = post?.postData;
+  const comments = post?.comments || [];
+
+  const {data: author} = getSingleUser(postData?.user_id);
+
+  if(isPostError) {
+    return <ErrorPage />
   }
-  const {postData, comments}: {postData: Post, comments: Comment[]} = post || {postData: {}, comments: []};
-  const {data: author} = getSingleUser(postData.user_id);
+
+  if(isPostLoading || !postData) {
+    return <SinglePostSkeleton />
+  }
   return (
     <>  
         <Layout direction='column'>
-            <Button label="На главную" onClick={() => navigate("/")}/>
-            <Layout flex={2}>
-                <Text view="ghost" size='xs'>{postData.id}</Text>
+            <Layout>
+                <Button size="s" iconLeft={IconArrowUndone} view="clear" label="На главную" onClick={() => navigate("/")}/>
+            </Layout>
+            <Layout flex={2} direction='column' >
+                <Text view="ghost" size='xs'>#{postData.id}</Text>
                 <Heading size='3xl'>{postData.title}</Heading>
                 <Text size='s'>Автор: {author?.name}</Text>
             </Layout>
@@ -46,7 +43,7 @@ const SinglePost = () => {
                 <Text size="m" lineHeight='m' as="p">{postData.body}</Text>
             </Layout>
             <Layout direction='column'>
-                {comments.map((comment) => {
+                {comments.map((comment: Comment) => {
                     return (
                         <Card key={comment.id} verticalSpace="s" horizontalSpace="s">
                             <Text size="m" lineHeight='m' as="p">{comment.body}</Text>
--- src/pages/singleUser/SingleUser.tsx
+++ src/pages/singleUser/SingleUser.tsx
@@ -1,29 +1,48 @@
 import { getSingleUser } from '@/shared/api/hooks/getSingleUser';
-import Heading from '@/shared/ui/Heading';
 import { Button } from '@consta/uikit/Button';
 import { Layout } from '@consta/uikit/Layout';
 import { Text } from '@consta/uikit/Text';
-import { Avatar } from '@consta/uikit/Avatar';
+import { User } from '@consta/uikit/User';
+import { IconArrowUndone } from '@consta/icons/IconArrowUndone';
 import { Badge } from '@consta/uikit/Badge';
 import { useNavigate, useParams } from 'react-router';
+import ErrorPage from '../error/ErrorPage';
+import SingleUserSkeleton from '@/shared/ui/SingleUserSceleton';
 
 const SingleUser = () => {
     const navigate = useNavigate();
   let params = useParams();
   const {data, isLoading, isError} = getSingleUser(Number(params.userId));
+
+  if(isError) {
+    return <ErrorPage />
+  }
+
+  if(isLoading) {
+    return <SingleUserSkeleton />
+  }
+
   return (
     <>
-        <Layout direction='column'>
-            <Button label="На главную" onClick={() => navigate("/")}/>
-            <Layout flex={1}>
-                <Avatar name={data?.name}/>
-                <Heading size='3xl'>{data?.name}</Heading>
+        <Layout direction='column' style={{padding: "20px 0", gap: "12px"}}>
+            <Layout>
+                <Button size="s" iconLeft={IconArrowUndone} view="clear" label="На главную" onClick={() => navigate("/")}/>
             </Layout>
-            <Badge label={data?.status} status={data?.status === "active" ? "success" : "system"}/>
             <Layout flex={1}>
-                <Text size="m" lineHeight='m' as="p">ID: {data?.id}</Text>
-                <Text size="m" lineHeight='m' as="p">Email: {data?.email}</Text>
-                <Text size="m" lineHeight='m' as="p">Gender: {data?.gender}</Text>
+                <User name={data?.name} info={data?.id} size='l' />
+            </Layout>
+            <Layout flex={1}>
+                <Badge label={data?.status} status={data?.status === "active" ? "success" : "system"}/>
+            </Layout>
+            <Layout flex={1} direction='column'>
+                <Layout flex={1} style={{gap: "1em", alignItems: "baseline"}}>
+                    <Text size="l" as="p" weight='bold'>Email:</Text>
+                    <Text size="m" as="p">{data?.email}</Text>
+                </Layout>
+                <Layout flex={1} style={{gap: "1em", alignItems: "baseline"}}>
+                    <Text size="l" as="p" weight='bold'>Gender: </Text>
+                    <Text size="m" as="p">{data?.gender}</Text>
+                </Layout>
             </Layout>
         </Layout>
     </>
